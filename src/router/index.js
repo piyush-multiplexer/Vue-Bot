876bb6e06718ce0baa87962f9898da33df861393
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-04-19 16:34:26 +0530

    home screen

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router =  new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: function () {
+        return import(/* webpackChunkName: "about" */ '../components/Home.vue')
+      },
+    }, {
+      path: '/list',
+      name: 'BotList',
+      component: function () {
+        return import(/* webpackChunkName: "about" */ '../components/BotList.vue')
+      },
+    }, {
+      path: '/:botId',
+      name: 'BotHome',
+      component: function () {
+        return import(/* webpackChunkName: "about" */ '../components/BotHome.vue')
+      },
+    },
+  ],
+})
+export default router
