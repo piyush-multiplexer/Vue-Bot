f6e7c3ab2b59b5b19089dbf3a234e16c40fbd9bf
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-04-20 17:56:46 +0530

    Password Widget Added

--- src/components/BotWidgets/PasswordWidget.vue
+++ src/components/BotWidgets/PasswordWidget.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="PasswordWidget">
-    <div v-if="showWidget" class="animated slideInUp">
+    <div v-if="showWidget" class="animated slideInUp" @keyup.enter="setPasswordValue">
       <v-layout row wrap>
         <v-flex xs10 md10>
-          <v-text-field class="bot-text-field" outline v-model="password" mask="##### #####"></v-text-field>
+          <v-text-field required ref="password" class="bot-text-field" outline v-model="password"
+                        type="password"></v-text-field>
         </v-flex>
         <v-flex xs2 md2>
-          <v-btn class="bot-button-round" @click="setPasswordValue" fab flat icon>
+          <v-btn class="bot-button-round" @click="setPasswordValue" :disabled="!password.length" fab flat icon>
             <v-icon style="transform:rotate(-45deg) ">send</v-icon>
           </v-btn>
         </v-flex>
@@ -32,12 +33,14 @@
     methods: {
       setPasswordValue () {
         let self = this
-        $(this.$el).addClass('animated fadeOutDown')
-        setTimeout(function () {
-          self.$parent.sendMessage(self.password)
-          self.$destroy()
-          self.$el.parentNode.removeChild(self.$el)
-        }, 500)
+        if (this.password.length) {
+          $(this.$el).addClass('animated fadeOutDown')
+          setTimeout(function () {
+            self.$parent.sendMessage(self.password)
+            self.$destroy()
+            self.$el.parentNode.removeChild(self.$el)
+          }, 500)
+        }
       },
     },
   }
