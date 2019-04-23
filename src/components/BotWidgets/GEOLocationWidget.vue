a0e59717704a04cf6efadcc9ae67270bbc733389
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-04-23 08:48:17 +0530

    MAP Widget | marker and click on map zoom fix to reset marker

--- src/components/BotWidgets/GEOLocationWidget.vue
+++ src/components/BotWidgets/GEOLocationWidget.vue
@@ -4,7 +4,7 @@
       <div class="widget-question">Drag & Drop Pin to Select Location</div>
       <v-layout row wrap>
         <v-flex xs10 md10>
-          <google-map ref="map" :center="center" :zoom="10" style="width: 100%; height: 500px">
+          <google-map ref="map" :center="center" :zoom="10" style="width: 100%; height: 500px" @click="test">
             <google-marker ref="marker" v-for="(marker,index) in markers" :key="index" :position="marker.position"
                            :clickable="true"
                            :draggable="true" @click="center=marker.position"
@@ -12,7 +12,8 @@
           </google-map>
         </v-flex>
         <v-flex xs2 md2 class="text-center">
-          <v-btn class="bot-button-round" :disabled="!locationObj.address.length" @click.once="setMapValue" fab flat icon>
+          <v-btn class="bot-button-round" :disabled="!locationObj.address.length" @click.once="setMapValue" fab flat
+                 icon>
             <v-icon style="transform:rotate(-45deg) ">send</v-icon>
           </v-btn>
         </v-flex>
@@ -49,16 +50,29 @@
       })
     },
     methods: {
-      getMarkerPosition (map) {
+      test (map) {
         let self = this
+        this.center = {
+          lat: map.latLng.lat(this.$refs.map),
+          lng: map.latLng.lng(this.$refs.map),
+        }
+        this.markers = [{ position: this.center }]
+        this.getLocationFromGoogle()
+      },
+      getMarkerPosition (map) {
         let latLong = {
           lat: map.latLng.lat(this.$refs.marker),
           lng: map.latLng.lng(this.$refs.marker),
         }
+        this.center = latLong
         this.locationObj.lat = latLong.lat
         this.locationObj.lng = latLong.lng
+        this.getLocationFromGoogle()
+      },
+      getLocationFromGoogle () {
+        let self = this
         NetworkCommunicator(`GET`,
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLong.lat},${latLong.lng}&key=AIzaSyCgG2RHYJmuCsaY0di7Jz3lEIEbsVhhWfY`).
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.center.lat},${this.center.lng}&key=AIzaSyCgG2RHYJmuCsaY0di7Jz3lEIEbsVhhWfY`).
           then(response => {
             if (response.results.length) {
               self.locationObj.address = response.results[0].formatted_address
