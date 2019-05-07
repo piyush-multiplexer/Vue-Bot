b6c35b4dee362df355e3ee7d6acdb5219b34a863
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-05-07 13:44:35 +0530

    animation changes

--- src/plugins/animateSendButton.js
+++ src/plugins/animateSendButton.js
@@ -1,32 +1,15 @@
 export function animateSendButton () {
-  // let elem = $('.bot-button-round-animate')[0]
-  // let elemClone = $(elem).clone()
-  // elem.style.visibility = 'visible'
+  let elem = $('.bot-button-round')[0]
+  $(elem).animate({ left: '20vh' })
   // move(elem).rotate(-90).end()
   // setTimeout(function () {
-  //   $(elem).animate({ bottom: '9vh' }, 200)
-  // }, 100)
+  //   $(elem).animate({ bottom: '14vh' }, 'slow')
+  // }, 10)
   //
   // setTimeout(function () {
   //   move(elem).rotate(-180).end()
-  // }, 150)
-  // setTimeout(function () {
-  //   $(elem).animate({ right: '65vh' }, 600)
   // }, 200)
   // setTimeout(function () {
-  //
-  //   elem = elemClone
-  // },100)
-  let elem = $('.bot-button-round')[0]
-  move(elem).rotate(-90).end()
-  setTimeout(function () {
-    $(elem).animate({ bottom: '14vh' }, 'slow')
-  }, 10)
-
-  setTimeout(function () {
-    move(elem).rotate(-180).end()
-  }, 200)
-  setTimeout(function () {
-    $(elem).animate({ right: '65vh' }, 'slow')
-  }, 550)
+  //   $(elem).animate({ right: '65vh' }, 'slow')
+  // }, 550)
 }
