bfbf34f67372738e0888ba5fb6c3fc0eff503deb
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-05-14 10:33:13 +0530

    added loader and powered by changes

--- src/components/BotUI.vue
+++ src/components/BotUI.vue
@@ -6,7 +6,7 @@
           <div id="bot-sub-content">
             <!--            <div :key="messageIndex" class="animated bounceInUp" v-for="(message,messageIndex) in messages">-->
             <div :key="messageIndex" v-for="(message,messageIndex) in messages">
-              <div style="width: 80%; margin: 0 0 0 auto" class="text-right animated fadeInRight"
+              <div class="text-right animated fadeInRight" style="width: 80%; margin: 0 0 0 auto"
                    v-if="message.type==='user'">
                 <!--              <div class="text-right animated bounceInRight" v-if="message.type==='user'">-->
                 <div class="message-content-user">
@@ -49,13 +49,14 @@
       <div class="p-t-5 p-b-5" id="powered-by">
         <v-layout class="text-center" row wrap>
           <v-flex xs12>
-            Powered By <img src="../../static/assets/image/MeraCRMlogo.png" width="60px">
+            Powered By <a href="https://www.meracrm.com"><img src="../../static/assets/image/MeraCRMlogo.png"
+                                                              width="60px"></a>
             <!--Powered By Avinashi Pvt. Ltd.-->
           </v-flex>
         </v-layout>
       </div>
     </div>
-    <div class="bot-loader">
+    <div class="bot-loader" v-if="screenLoader">
       <div class="bouncing-loader">
         <div></div>
         <div></div>
@@ -67,103 +68,105 @@
 </template>
 
 <script>
-    import TextWidget from './BotWidgets/TextWidget'
-    import ButtonsWidget from './BotWidgets/ButtonsWidget'
-    import CalendarWidget from './BotWidgets/CalendarWidget'
-    import DatePickerWidget from './BotWidgets/DatePickerWidget'
-    import TimePickerWidget from './BotWidgets/TimePickerWidget'
-    import SearchSuggestion from './BotWidgets/SearchSuggestion'
-    import CardSliderWidget from './BotWidgets/CardSliderWidget'
-    import RatingWidget from './BotWidgets/RatingWidget'
-    import RedirectWidget from './BotWidgets/RedirectWidget'
-    import FileUploadWidget from './BotWidgets/FileUploadWidget'
-    import GEOLocationWidget from './BotWidgets/GEOLocationWidget'
-    import PhoneWidget from './BotWidgets/PhoneWidget'
-    import PasswordWidget from './BotWidgets/PasswordWidget'
-    import EmailWidget from './BotWidgets/EmailWidget'
-    import UserNameWidget from './BotWidgets/UserNameWidget'
-    import NoInputWidget from './BotWidgets/NoInputWidget'
-    import UserBubbleWidget from './BotWidgets/UserBubbleWidget'
-    import CustomTextWidget from './BotWidgets/CustomTextWidget'
-    import SelfieWidget from './BotWidgets/SelfieWidget'
+  import TextWidget from './BotWidgets/TextWidget'
+  import ButtonsWidget from './BotWidgets/ButtonsWidget'
+  import CalendarWidget from './BotWidgets/CalendarWidget'
+  import DatePickerWidget from './BotWidgets/DatePickerWidget'
+  import TimePickerWidget from './BotWidgets/TimePickerWidget'
+  import SearchSuggestion from './BotWidgets/SearchSuggestion'
+  import CardSliderWidget from './BotWidgets/CardSliderWidget'
+  import RatingWidget from './BotWidgets/RatingWidget'
+  import RedirectWidget from './BotWidgets/RedirectWidget'
+  import FileUploadWidget from './BotWidgets/FileUploadWidget'
+  import GEOLocationWidget from './BotWidgets/GEOLocationWidget'
+  import PhoneWidget from './BotWidgets/PhoneWidget'
+  import PasswordWidget from './BotWidgets/PasswordWidget'
+  import EmailWidget from './BotWidgets/EmailWidget'
+  import UserNameWidget from './BotWidgets/UserNameWidget'
+  import NoInputWidget from './BotWidgets/NoInputWidget'
+  import UserBubbleWidget from './BotWidgets/UserBubbleWidget'
+  import CustomTextWidget from './BotWidgets/CustomTextWidget'
+  import SelfieWidget from './BotWidgets/SelfieWidget'
 
-    export default {
-        name: 'BotUI',
-        components: {
-            UserBubbleWidget,
-            'text_widget': TextWidget,
-            'buttons_vertical': ButtonsWidget,
-            'calendar_time': CalendarWidget,
-            'date_roll': DatePickerWidget,
-            'time_roll': TimePickerWidget,
-            'search_suggester_dynamic': SearchSuggestion,
-            'card_slide': CardSliderWidget,
-            'star_rating': RatingWidget,
-            'redirect_convflow': RedirectWidget,
-            'file_upload': FileUploadWidget,
-            'geo_location': GEOLocationWidget,
-            'user_phone_number_10': PhoneWidget,
-            'user_phone_number': PhoneWidget,
-            'user_password': PasswordWidget,
-            'user_email': EmailWidget,
-            'user_name': UserNameWidget,
-            'user_fullname': UserNameWidget,
-            'no_input': NoInputWidget,
-            'custom_text': CustomTextWidget,
-            'take_photo': SelfieWidget,
-        },
-        data() {
-            return {
-                messages: [],
-                historyId: '',
-                userValue: '',
-                isBotLoading: false,
-                conversationEnd: false,
-                botConfig: {isHuman: true},
-            }
-        },
-        async mounted() {
-            let self = this
-            this.messages = []
-            this.messages[0] = await Bot.getConfig()
-            this.historyId = await Bot.getHistoryId()
-            window.BotMetaData = await Bot.getBotHashData()
-            this.$forceUpdate()
-        },
-        computed: {
-            showInput() {
-                return this.messages[this.messages.length - 1].input.name === 'text'
-            },
-        },
-        methods: {
-            appendMessage(message) {
-                let self = this
-                this.isBotLoading = true
-                this.messages.push(message)
-                this.isBotLoading = false
-            },
-            toggleInput() {
-                this.conversationEnd = !this.conversationEnd
-            },
-            hideWidget(widgetId) {
-                console.log($('#' + widgetId).hide())
-            },
-            async sendMessage(userValue) {
-                this.messages.push({
-                    type: 'user',
-                    input: {name: userValue.type},
-                    class: '',
-                    text: [{string: this.userValue ? this.userValue : userValue.value}],
-                    meta: userValue.meta,
-                })
-                if (userValue === 'skipped')
-                    this.messages.push(await Bot.getNextMessage('', ''))
-                else
-                    this.messages.push(
-                        await Bot.getNextMessage(this.userValue ? this.userValue : userValue.value, userValue.metaValue))
-                this.userValue = ''
-                this.$forceUpdate()
-            },
-        },
-    }
+  export default {
+    name: 'BotUI',
+    components: {
+      UserBubbleWidget,
+      'text_widget': TextWidget,
+      'buttons_vertical': ButtonsWidget,
+      'calendar_time': CalendarWidget,
+      'date_roll': DatePickerWidget,
+      'time_roll': TimePickerWidget,
+      'search_suggester_dynamic': SearchSuggestion,
+      'card_slide': CardSliderWidget,
+      'star_rating': RatingWidget,
+      'redirect_convflow': RedirectWidget,
+      'file_upload': FileUploadWidget,
+      'geo_location': GEOLocationWidget,
+      'user_phone_number_10': PhoneWidget,
+      'user_phone_number': PhoneWidget,
+      'user_password': PasswordWidget,
+      'user_email': EmailWidget,
+      'user_name': UserNameWidget,
+      'user_fullname': UserNameWidget,
+      'no_input': NoInputWidget,
+      'custom_text': CustomTextWidget,
+      'take_photo': SelfieWidget,
+    },
+    data () {
+      return {
+        messages: [],
+        historyId: '',
+        userValue: '',
+        isBotLoading: false,
+        conversationEnd: false,
+        botConfig: { isHuman: true },
+        screenLoader: true,
+      }
+    },
+    async mounted () {
+      let self = this
+      this.messages = []
+      this.messages[0] = await Bot.getConfig()
+      this.historyId = await Bot.getHistoryId()
+      window.BotMetaData = await Bot.getBotHashData()
+      this.screenLoader = false
+      this.$forceUpdate()
+    },
+    computed: {
+      showInput () {
+        return this.messages[this.messages.length - 1].input.name === 'text'
+      },
+    },
+    methods: {
+      appendMessage (message) {
+        let self = this
+        this.isBotLoading = true
+        this.messages.push(message)
+        this.isBotLoading = false
+      },
+      toggleInput () {
+        this.conversationEnd = !this.conversationEnd
+      },
+      hideWidget (widgetId) {
+        console.log($('#' + widgetId).hide())
+      },
+      async sendMessage (userValue) {
+        this.messages.push({
+          type: 'user',
+          input: { name: userValue.type },
+          class: '',
+          text: [{ string: this.userValue ? this.userValue : userValue.value }],
+          meta: userValue.meta,
+        })
+        if (userValue === 'skipped')
+          this.messages.push(await Bot.getNextMessage('', ''))
+        else
+          this.messages.push(
+            await Bot.getNextMessage(this.userValue ? this.userValue : userValue.value, userValue.metaValue))
+        this.userValue = ''
+        this.$forceUpdate()
+      },
+    },
+  }
 </script>
