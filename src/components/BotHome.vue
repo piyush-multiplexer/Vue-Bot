af7a113060ef78586687d44534879ec8cd44c5e5
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-07-20 14:51:32 +0530

    floating button to hide/show bot ongoing

--- src/components/BotHome.vue
+++ src/components/BotHome.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home bot-home">
-    <BotUI v-if="isBot"></BotUI>
+  <div class="home bot-home" id="botHome" style="right: 7px;position: absolute;">
+    <v-btn @click="handleClick" absolute bottom color="blue" dark fab id="accordion" left small>
+      <v-icon>add</v-icon>
+    </v-btn>
+    <div id="panel">
+      <BotUI v-if="isBot"></BotUI>
+    </div>
+
   </div>
 </template>
 
@@ -22,6 +28,19 @@
       else
         window.Bot = new VueBot(this.$route.params.botId)
       this.isBot = true
+    },
+    methods: {
+      handleClick () {
+        if (this.$route.query && this.$route.query.collapsed == 'true') {
+          this.$route.query.collapsed = 'false'
+          console.log(this.$route.query)
+          $('#panel').toggle()
+        } else {
+          this.$route.query.collapsed = 'true'
+          console.log(this.$route.query)
+          $('#panel').toggle()
+        }
+      },
     },
   }
 </script>
